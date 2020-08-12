e3356a7685bf5d4d06c9f2746125d1b9c713ef95
Edward Harman <eharman@tripadvisor.com>
2020-08-12 16:13:51 -0400

    create symlink

--- src/main/java/org/ethelred/log_helper/App.java
+++ src/main/java/org/ethelred/log_helper/App.java
@@ -1,6 +1,13 @@
 /* (C) 2020 Edward Harman */
 package org.ethelred.log_helper;
 
+import static java.nio.file.Files.createSymbolicLink;
+import static java.nio.file.Files.delete;
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.isSameFile;
+import static java.nio.file.Files.isSymbolicLink;
+import static java.nio.file.Files.readSymbolicLink;
+
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
@@ -9,9 +16,11 @@ import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.core.util.StatusPrinter;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -44,6 +53,7 @@ public class App implements Callable<Integer> {
 
   @Parameters(index = "0")
   public void setOutput(Path output) {
+    original = output;
     Path parent = output.getParent();
     if (parent != null) {
       dir = parent.toString() + "/";
@@ -70,8 +80,8 @@ public class App implements Callable<Integer> {
     }
   }
 
+  private Path original;
   private String dir = "";
-
   private String name;
   private String extension;
 
@@ -81,6 +91,48 @@ public class App implements Callable<Integer> {
       description = "log to console as well as file")
   private boolean console;
 
+  @Option(
+      names = {"-l", "--link"},
+      defaultValue = "true",
+      description = "Maintain original name as symbolic link to latest log file")
+  private boolean link;
+
+  private static final long TIME_BETWEEN_CHECKS = TimeUnit.MINUTES.toMillis(1);
+
+  private class LinkerPolicy<T> extends TimeBasedRollingPolicy<T> {
+    private volatile long lastCheck = 0;
+
+    @Override
+    public String getActiveFileName() {
+      String activeName = super.getActiveFileName();
+      _checkLink(activeName);
+      return activeName;
+    }
+
+    private void _checkLink(String activeName) {
+      if (link) {
+        long now = System.currentTimeMillis();
+        if (now > lastCheck + TIME_BETWEEN_CHECKS) {
+          lastCheck = now;
+          try {
+            Path active = Path.of(activeName).toAbsolutePath();
+            if (exists(original)
+                && isSymbolicLink(original)
+                && !isSameFile(readSymbolicLink(original), active)) {
+              delete(original);
+            }
+            if (!exists(original)) {
+              createSymbolicLink(original.toAbsolutePath(), active);
+            }
+          } catch (IOException e) {
+            System.err.println("Exception updating link");
+            e.printStackTrace(System.err);
+          }
+        }
+      }
+    }
+  }
+
   private Logger configureLogging() {
     LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
     PatternLayoutEncoder ple = new PatternLayoutEncoder();
@@ -89,7 +141,7 @@ public class App implements Callable<Integer> {
     ple.setContext(lc);
     ple.start();
 
-    TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<>();
+    TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new LinkerPolicy<>();
     rollingPolicy.setFileNamePattern(dir + name + ".%d." + extension + ".gz");
     rollingPolicy.setMaxHistory(8);
     rollingPolicy.setContext(lc);
--- src/test/groovy/org/ethelred/log_helper/AppTest.groovy
+++ src/test/groovy/org/ethelred/log_helper/AppTest.groovy
@@ -1,8 +1,76 @@
 
 package org.ethelred.log_helper
 
+import picocli.CommandLine
 import spock.lang.Specification
 
+import java.nio.file.Path
+import static java.nio.file.Files.*
+
 class AppTest extends Specification {
-    
+    Path testDir
+    def cmd
+
+    def setup() {
+        testDir = createTempDirectory(getClass().simpleName)
+        cmd = new CommandLine(new App())
+    }
+
+    def cleanup() {
+        testDir.deleteDir()
+    }
+
+    def "test the default behaviour"() {
+        setup:
+        def base = "test1"
+        def ext = ".log"
+        def logPath = testDir.resolve(base + ext)
+        def testLine = "Hello, world!"
+
+        when:
+        withInput(testLine + "\n") {
+            cmd.execute(logPath.toString())
+        }
+
+        then:
+        isDirectory(testDir)
+        isSymbolicLink(logPath)
+
+        when:
+        def children = children(testDir)
+
+        then:
+        children.size() == 2
+
+        when:
+        def logFile = children.find { isRegularFile(it)}
+
+        then:
+        logFile != null
+        "${logFile.fileName}".startsWith(base)
+        "${logFile.fileName}".endsWith(ext)
+        isSameFile(logFile, readSymbolicLink(logPath))
+
+        when:
+        def lines = logFile.readLines()
+
+        then:
+        lines.size() == 1
+        lines.first().contains(testLine)
+    }
+
+    static def withInput(String text, Closure closure) {
+        def oldIn = System.in
+        try {
+            def ba = new ByteArrayInputStream(text.bytes)
+            System.setIn(ba)
+            closure()
+        } finally {
+            System.setIn(oldIn)
+        }
+    }
+
+    static def children(Path dir) {
+        newDirectoryStream(dir).collect()
+    }
 }
